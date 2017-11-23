626f46ca473b0f56f7c46af58c9e2f587bdee748
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-11-23 10:07:37 -0500

    Distance Between Point and Line (In Progress)

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -21,3 +21,35 @@ import Foundation
 */
 
 // Begin here... once you teach the computer to do this, you will never need to do it by hand again! :)
+
+// Create Line structure
+struct Line {
+    var x : Double = 0.0
+    var y : Double = 0.0
+}
+
+// Create Point structure
+struct Point {
+    (10.0, 5.0) to (20.0, 10.0)
+}
+
+
+// Create the points here
+var pointStart = Point()
+var pointEnd = Point(x: 3.0, y: 4.0)
+
+
+// Query the points here to find the x and y values separately
+pointStart.x
+pointStart.y
+pointEnd.x
+pointEnd.y
+
+
+// Define the distance function of the point here
+func distanceBetweenLineAndPoint(from: Point, to: Point) -> Double {
+    return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2))
+}
+
+// Invoke the functions here
+distanceBetweenLineAndPoint(from: pointStart, to: pointEnd)
